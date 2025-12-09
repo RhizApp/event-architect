a1d04fa13e02984ffb03603ac01c6b56bd3a2cc9
[ISW] <is@werhiz.com>
2025-12-09 18:54:26 -0500

    fix(middleware): hardcode clerk publishable key to resolve 500 error

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publishableKey: "pk_test_bWVldC1oeWVuYS01NC5jbGVyay5hY2NvdW50cy5kZXYk",
+});
 
 export const config = {
   matcher: [
